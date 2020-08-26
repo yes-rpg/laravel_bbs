f6244008b766d232b517ef882fa223a1e0cc907f
付康辉 <15009245293@163.com>
2020-08-27 00:09:54 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',['except' => 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -59,6 +64,7 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
@@ -72,6 +78,7 @@ class UsersController extends Controller
      */
     public function update(UserRequest $request, User $user,ImageUploadHandler $uploadHandler)
     {
+        $this->authorize('update',$user);
         $data = $request->all();
         if($request->avatar)
         {
