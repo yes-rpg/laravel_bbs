4e86922dc0912a3cc69dcbd3b20da99ca1d0047c
付康辉 <15009245293@163.com>
2020-08-30 15:26:32 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -17,4 +17,28 @@ class Topic extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function scopeWithOrder($query,$order){
+        //不同的排序使用不同的排序逻辑
+        switch ($order){
+            case "recent":
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+        }
+    }
+
+    public function scopeRecentReplied($query)
+    {
+        //当话题有新回复时,我们将编写逻辑来更新话题模型的reply_count属性
+        //此时会自动触发框架对数据模型的updated_at时间戳的更新
+        return $query->orderBy('updated_at','desc');
+    }
+
+    public function recent($query)
+    {
+        //按照创建时间排序
+        return $query->orderBy('created_at','desc');
+    }
+
 }
