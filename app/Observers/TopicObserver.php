c6efb3e5676b9dfd2dcd2639ce683a8111afe36a
付康辉 <15009245293@163.com>
2020-09-03 15:16:26 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,7 +2,7 @@
 
 namespace App\Observers;
 
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -15,10 +15,16 @@ class TopicObserver
         $topic->body = clean($topic->body,'user_topic_body');
 //            $topic->body = $topic->body.'这是事件监听添加得内容';
         $topic->excerpt = make_excerpt($topic->body);
+    }
 
-        if(!$topic->slug)
-        {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+    public function saved(Topic $topic)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+        if (!$topic->slug) {
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
+
 }
